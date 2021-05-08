f353de45c3a6f2fb7c23615eb679c79d79ff8fcc
Eason0210 <aqua0210@163.com>
2021-05-09 01:11:23 +0800

    add wubi86_jidian

--- rime.lua
+++ rime.lua
@@ -3,3 +3,10 @@
 
 -- 需要以词定字插件打开第5行注释
 -- select_character_processor = require("select_character")
+
+-- 获取日期、时间和周
+date_translator = require("date_time_week")
+
+-- single_char_filter: 候选项重排序，使单字优先
+-- 详见 `lua/single_char.lua`
+single_char_filter = require("single_char")
